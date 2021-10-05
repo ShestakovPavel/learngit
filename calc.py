73c16c241dbb39a808dd7adf39394f3a8117c5f1
ShestakovPavel <91872264+ShestakovPavel@users.noreply.github.com>
2021-10-05 17:44:50 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,22 @@
+#simple calculator
+
+
+def sum(a,b):
+	a, b = int(a), int(b)
+	return a + b
+
+def sub(a,b):
+	pass
+
+def multipli(a,b):
+	pass
+
+def division(a,b):
+	pass
+
+print("Введите два чиcла и укажите операцию")
+num_a = input("num 1: ")
+num_b = input("num 2: ")
+operation = input("Укажите операцию(+, - , * или /) ")
+if operation == '+':
+	print(sum(num_a, num_b))
